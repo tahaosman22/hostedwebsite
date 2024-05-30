283f72b9616504021c7405cdc6f73a2d9786a45f
taha <taha@TOsman-iMac.local>
2024-05-30 20:27:35 +0100

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,12 @@
-import "@/styles/globals.css";
+import "../styles/globals.css";
+import { AnimatePresence } from "framer-motion";
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+function MyApp({ Component, pageProps, router }) {
+  return (
+    <AnimatePresence initial={false} mode="wait">
+      <Component key={router.pathname} {...pageProps} />
+    </AnimatePresence>
+  );
 }
+
+export default MyApp;
